0777bec5e5d4792ea43615380b503a5312a70a70
Abhishek12589 <87072119+Abhishek12589@users.noreply.github.com>
2021-10-09 16:57:50 +0530

    Add files via upload

---
+++ greater of two no.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter 1st no.";
+    cin>>a;
+    cout<<"Enter 2st no.";
+    cin>>b;
+    cout<<"Greater no is = "<<max(a,b);
+    return 0;
+}
